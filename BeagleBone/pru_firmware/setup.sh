495e33c858fc7e2443dc74bd42bb13d148b956e0
Canela-san <sstarksan@gmail.com>
2026-07-05 22:09:33 -0300

    update software

--- BeagleBone/pru_firmware/setup.sh
+++
@@ -1,7 +0,0 @@
-config-pin P9_31 pruout
-config-pin P9_29 pruout
-config-pin P9_30 pruin
-config-pin P9_28 pruout
-sudo cp teste_spi_pru.out /lib/firmware/am335x-pru0-fw
-echo stop | sudo tee /sys/class/remoteproc/remoteproc1/state
-echo start | sudo tee /sys/class/remoteproc/remoteproc1/state
